944dc7759d415e4ee1999d10cf5b53085e698b01
nopal <naufaladyat234@gmail.com>
2023-10-28 20:14:14 +0700

    neat files

--- Scripts/Sound/AwakeScript.cs
+++ Scripts/Sound/AwakeScript.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class AwakeScript : MonoBehaviour
 {
     private const string VOLUME_BGM = "volume";
+    private const string VOLUME_SFX = "volume";
+
 
     // Start is called before the first frame update
     void Awake()
     {
         PlayerPrefs.SetFloat(VOLUME_BGM, 1f);
+        PlayerPrefs.SetFloat(VOLUME_SFX, 1f);
+
 
     }
 }
